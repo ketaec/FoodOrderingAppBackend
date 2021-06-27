d22101acd90a910a2d3101722a7bf70f8cbb75a4
KetaEs <KetaEs@dev.azure.com>
2021-06-27 08:31:19 +0200

    added customer entity

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CustomerEntity.java
@@ -0,0 +1,139 @@
+package com.upgrad.FoodOrderingApp.service.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+@Entity
+@Table(name = "customer")
+@NamedQueries({
+        @NamedQuery(name = "customerByUUID", query = "select ce from CustomerEntity ce where ce.uuid = :uuid"),
+        @NamedQuery(name = "customerByContactNumber", query = "select ce from CustomerEntity ce where ce.contactNumber = :contactNumber")
+
+})
+public class CustomerEntity implements Serializable {
+
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "uuid")
+    @Size(max = 200)
+    @NotNull
+    private String uuid;
+
+    @Column(name = "firstname")
+    @NotNull
+    @Size(max = 30)
+    private String firstName;
+
+    @Column(name = "lastname")
+    @Size(max = 30)
+    private String lastName;
+
+    @Column(name = "email")
+    @Size(max = 50)
+    private String email;
+
+    @Column(name = "contact_number")
+    @Size(max = 30)
+    @NotNull
+    private String contactNumber;
+
+    @Column(name = "password")
+    @NotNull
+    @Size(max = 255)
+    private String password;
+
+    @Column(name = "salt")
+    @NotNull
+    @Size(max = 255)
+    private String salt;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return new EqualsBuilder().append(this, obj).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(this).hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+}
+
