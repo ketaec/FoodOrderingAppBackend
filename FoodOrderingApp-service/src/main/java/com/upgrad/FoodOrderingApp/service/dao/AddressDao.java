3e098505e1e46f832e5f2e25dc31bdd88449a28e
KetaEs <KetaEs@dev.azure.com>
2021-06-30 15:21:31 +0200

    added getAddressByUUID and delete Address dao

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/AddressDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/AddressDao.java
@@ -4,6 +4,7 @@ import com.upgrad.FoodOrderingApp.service.entity.AddressEntity;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
 @Repository
@@ -15,5 +16,19 @@ public class AddressDao {
     public AddressEntity saveAddress(final AddressEntity address) {
         entityManager.persist(address);
         return address;
+    }
+
+    public AddressEntity getAddressByUuid(String uuid){
+        try{
+            AddressEntity addressEntity = entityManager.createNamedQuery("getAddressByUuid",AddressEntity.class).setParameter("uuid",uuid).getSingleResult();
+            return addressEntity;
+        }catch (NoResultException nre){
+            return null;
+        }
+    }
+
+    public AddressEntity deleteAddress(AddressEntity addressEntity) {
+        entityManager.remove(addressEntity);
+        return addressEntity;
     }
 }
