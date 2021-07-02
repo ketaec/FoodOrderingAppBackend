0f9ea2be697d1f727491d61fbd39056326a1bf3f
Eswar Chand Keta <eswarchandketa@gmail.com>
2021-07-02 16:31:42 +0200

    added  getCategoryById in dao

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CategoryDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CategoryDao.java
@@ -1,4 +1,24 @@
 package com.upgrad.FoodOrderingApp.service.dao;
 
+import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+@Repository
 public class CategoryDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public CategoryEntity getCategoryById(final String uuid) {
+        try {
+            CategoryEntity categoryEntity = entityManager.createNamedQuery("categoryByUuid",CategoryEntity.class).setParameter("uuid",uuid).getSingleResult();
+            return categoryEntity;
+        }catch (NoResultException nre){
+            return null;
+        }
+    }
 }
