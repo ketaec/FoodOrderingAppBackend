4ad8647e56d9b872d2a7ceb334485c0fe88d3ed9
Eswar Chand Keta <eswarchandketa@gmail.com>
2021-07-04 08:35:42 +0200

    added get category services

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CategoryService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CategoryService.java
@@ -4,6 +4,7 @@ import com.upgrad.FoodOrderingApp.service.dao.CategoryDao;
 import com.upgrad.FoodOrderingApp.service.dao.RestaurantDao;
 import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
 import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import com.upgrad.FoodOrderingApp.service.exception.CategoryNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,10 +18,32 @@ public class CategoryService {
     @Autowired
     private RestaurantDao restaurantDao;
 
+    @Autowired
+    private CategoryDao categoryDao;
+
     public List<CategoryEntity> getCategoriesByRestaurant(String restaurantUUID) {
         RestaurantEntity restaurantEntity = restaurantDao.getRestaurantByUUID(restaurantUUID);
         return restaurantEntity.getCategories().stream()
                 .sorted(Comparator.comparing(CategoryEntity::getCategoryName))
                 .collect(Collectors.toList());
+    }
+
+    public List<CategoryEntity> getAllCategoriesOrderedByName() {
+        return categoryDao.getAllCategories().stream().sorted(Comparator.comparing(CategoryEntity::getCategoryName))
+                .collect(Collectors.toList());
+    }
+
+    public CategoryEntity getCategoryById(String categoryUuid) throws CategoryNotFoundException {
+        if (categoryUuid== null || categoryUuid.isEmpty()) {
+            throw new CategoryNotFoundException("CNF-001", "Category id field should not be empty");
+        }
+
+        CategoryEntity categoryEntity = categoryDao.getCategoryById(categoryUuid);
+
+        if (categoryEntity == null) {
+            throw new CategoryNotFoundException("CNF-002", "No category by this id");
+        }
+
+        return categoryEntity;
     }
 }
