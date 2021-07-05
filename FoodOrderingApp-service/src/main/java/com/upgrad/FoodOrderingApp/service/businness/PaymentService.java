a7690d9e4736cdc7afff6a90cd3aa5df7abbc2b8
Eswar Chand Keta <eswarchandketa@gmail.com>
2021-07-05 07:31:54 +0200

    added getPaymentByUUID methods in payment service and payment dao

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/PaymentService.java
@@ -0,0 +1,24 @@
+package com.upgrad.FoodOrderingApp.service.businness;
+
+import com.upgrad.FoodOrderingApp.service.dao.PaymentDao;
+import com.upgrad.FoodOrderingApp.service.entity.PaymentEntity;
+import com.upgrad.FoodOrderingApp.service.exception.PaymentMethodNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PaymentService {
+
+    @Autowired
+    private PaymentDao paymentDao;
+
+    public PaymentEntity getPaymentByUUID(final String paymentId) throws PaymentMethodNotFoundException {
+        PaymentEntity payment = paymentDao.getPaymentByPaymentId(paymentId);
+        if(payment == null){
+            throw new PaymentMethodNotFoundException("PNF-002", "No payment method found by this id");
+        }
+        return payment;
+    }
+}
