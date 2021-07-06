65817a7113c2ff9764d5785b5d68a6632fbaea9e
KetaEs <KetaEs@dev.azure.com>
2021-07-06 12:00:30 +0200

    re arranged constructor class params in order entity

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/OrderEntity.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/OrderEntity.java
@@ -60,7 +60,7 @@ public class OrderEntity implements Serializable {
 
     }
 
-    public OrderEntity(@Size(max = 200) String uuid, double bill, double discount, Date date, CouponEntity coupon, RestaurantEntity restaurant, PaymentEntity payment, CustomerEntity customer, AddressEntity address) {
+    public OrderEntity(@Size(max = 200) String uuid, double bill, CouponEntity coupon, double discount, Date date,  PaymentEntity payment, CustomerEntity customer, AddressEntity address, RestaurantEntity restaurant) {
         this.uuid = uuid;
         this.bill = bill;
         this.discount = discount;
